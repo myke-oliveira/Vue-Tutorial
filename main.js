cb52c14cd25c3eacd5aa6bd03368d86feddfda76
Myke Albuquerque Pinto de Oliveira <myke.oliveira@monetizze.com.br>
2020-03-05 18:20:04 -0300

    In Stock feature

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var app = new Vue({
 	data: {
 		product: 'Socks',
 		description: 'This is a great product. The best one in the web.',
-		image: './image/vmSocks-green-onWhite.jpg'
+		image: './image/vmSocks-green-onWhite.jpg',
+		url: 'https://www.netshoes.com.br/',
+		inStock: true
 	}
 })
