9463915278f63cd3a38cecfc0e59c72a50fb2e3f
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-03-06 13:22:22 -0300

    Prettify code

--- main.js
+++ main.js
@@ -1,40 +1,52 @@
 var app = new Vue({
-	el: '#app',
-	data: {
-		product: 'Socks',
-		description: 'This is a great product. The best one in the web.',
-		image: './image/vmSocks-green-onWhite.jpg',
-		url: 'https://www.netshoes.com.br/',
-		inventory: -1,
-		onSale: true,
-		details: [
-			"80% cotton",
-			"20% polyester",
-			"Gender-neutral"
-		],
-		variants: [
-			{
-				id: 2234,
-				color: 'green'
-			},
-			{
-				id: 2235,
-				color: 'blue'
-			}
-		],
-		sizes: [
-			'P', 'M', 'G'
-		],
-        cart: 0
-	}
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'This is a great product. The best one in the web.',
+        image: './image/vmSocks-green-onWhite.jpg',
+        url: 'https://www.netshoes.com.br/',
+        inventory: -1,
+        onSale: true,
+        details: [
+            "80% cotton",
+            "20% polyester",
+            "Gender-neutral"
+        ],
+        variants: [{
+                id: 2234,
+                color: 'green',
+                image: './image/vmSocks-green-onWhite.jpg'
+            },
+            {
+                id: 2235,
+                color: 'blue',
+                image: './image/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+        sizes: [
+            'P', 'M', 'G'
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1;
+        },
+        removeFromCart: function() {
+            this.cart -= 1;
+        },
+        updateProduct: function(image) {
+            this.image = image;
+        }
+    }
 });
 
 var menuApp = new Vue({
-	el: '#menu',
-	data: {
+    el: '#menu',
+    data: {
         menus: null,
     }
 });
 
 axios.get('http://localhost:8090/getAsideMenu/')
-.then(response => (menuApp.menus = response.data));
+    .then(response => (menuApp.menus = response.data));
